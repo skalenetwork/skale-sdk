46e04a4dc1ab7cd5510057fa7bb8ab8eaf68091e
Alex Sheverdin <alex@skalelabs.com>
2021-02-19 18:02:40 +0200

    SKALE-3883 Bump skaled version to 3.5.3-plus-ws4

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@
 ###############################################################################
 
 _USE_DEBUG=0
-_SKALED_IMAGE="skalenetwork/schain:3.5.2-asan-ws-fix"
+_SKALED_IMAGE="skalenetwork/schain:3.5.3-plus-ws4"
 _DATA_DIR="./data_dir"
 
 ###############################################################################
