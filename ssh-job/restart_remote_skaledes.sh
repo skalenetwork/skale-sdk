27dc521cace116e4ad3e0a6761021bd6a1d56bba
Alex Sheverdin <alex@skalelabs.com>
2021-03-04 12:37:03 +0200

    SKALE-3883 Add script to restart skaled on all sChain nodes

---
+++ ssh-job/restart_remote_skaledes.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+IPS=()
+while read line ; do
+        IPS+=($line)
+done <ips.txt
+
+NUM_NODES=${#IPS[@]}
+echo $NUM_NODES
+
+for IP in ${IPS[*]} #:0:11}
+do
+
+	I=$((I+1))
+	echo $I
+#	echo $IP
+	CUR_URL="http://$IP:1234"
+  echo $CUR_URL
+## Get Block Number
+#	curl -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $CUR_URL
+
+## Turn off broadcast
+#	curl -X POST --data '{"jsonrpc":"2.0","method":"debug_pauseBroadcast","params":[true],"id":1}' $CUR_URL
+CONT=skale-ci-$I-0
+echo $CONT
+ssh -i ~/.ssh/ash_paris.pem -o "StrictHostKeyChecking no" ubuntu@$IP <<- ****
+sudo docker restart $CONT
+
+****
+echo "Skaled restarted on $IP"
+
+done
